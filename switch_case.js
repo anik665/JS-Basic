def9a28220bf7489316e71e724e9de0e6b3fe051
anik665 <97697517+anik665@users.noreply.github.com>
2025-04-13 20:09:54 +0600

    Switch-case

---
+++ switch_case.js
@@ -0,0 +1,19 @@
+let salary=30000;
+switch(salary){
+    case 5000:
+        console.log("You are very child for married");
+        break;
+    case 10000 :
+        console.log("You need to work hard for married");
+        break;
+    case 20000:
+        console.log("You are little bit ready for married");
+        break;
+    case 30000:
+        console.log("You are ready for married");
+        break;
+    default:
+        console.log("You are not ready for married");
+            break
+                     
+}
